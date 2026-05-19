9b862e78d3b47979bef74c040433263ac1a16af8
Michelle Altamirano <michelle.altamirano@udla.edu.ec>
2026-05-19 17:43:52 -0500

    Primer commit:Funcion para pedir las calificaciones y validar el rango de 0 a 10

---
+++ calpromedio.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Funcion para pedir las calificaciones y validar el rango de 0 a 10
+void cargarNotas(int n_estudiantes, int n_materias, float calificaciones[][n_materias]) {
+    for (int i = 0; i < n_estudiantes; i++) {
+        printf("\nNotas del Estudiante %d\n", i + 1);
+        for (int j = 0; j < n_materias; j++) {
+            float nota_ingresada;
+            do {
+                printf("Asignatura %d (0-10): ", j + 1);
+                scanf("%f", &nota_ingresada);
+                if (nota_ingresada < 0 || nota_ingresada > 10) {
+                    printf("Error: La nota debe estar entre 0 y 10. Intenta otra vez.\n");
+                }
+            } while (nota_ingresada < 0 || nota_ingresada > 10);
+            
+            // Guardar usando aritmetica de punteros
+            *(*(calificaciones + i) + j) = nota_ingresada;
+        }
+    }
+}
