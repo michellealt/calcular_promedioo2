5debac8009d3dfa74a6bfe38ef047817457708b4
Michelle Altamirano <michelle.altamirano@udla.edu.ec>
2026-05-20 16:35:56 -0500

    Quinto commit:Encontrar la calificacion mas alta y baja por asignatura

--- calpromedio.c
+++ calpromedio.c
@@ -60,3 +60,18 @@ void notaAltaBajaEstudiante(int n_estudiantes, int n_materias, float calificacio
         printf("Estudiante %d -> Nota Mas Alta: %.2f | Nota Mas Baja: %.2f\n", i + 1, alta, baja);
     }
 }
+// Encontrar la calificacion mas alta y baja por asignatura
+void notaAltaBajaAsignatura(int n_estudiantes, int n_materias, float calificaciones[][n_materias]) {
+    printf("\nNOTA ALTA Y BAJA POR ASIGNATURA\n");
+    for (int j = 0; j < n_materias; j++) {
+        float alta = ((calificaciones + 0) + j);
+        float baja = ((calificaciones + 0) + j);
+        
+        for (int i = 1; i < n_estudiantes; i++) {
+            float actual = ((calificaciones + i) + j);
+            if (actual > alta) alta = actual;
+            if (actual < baja) baja = actual;
+        }
+        printf("Asignatura %d -> Nota Mas Alta: %.2f | Nota Mas Baja: %.2f\n", j + 1, alta, baja);
+    }
+}
